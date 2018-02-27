6af1a45da42a13518576155f6feaa3fd984e54b0
Paquin, Wyatt A <001382793@n101-a7-mac.local>
2018-02-27 17:49:49 -0500

    Initial Commit

---
+++ iOS-Weather-App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iOS-Weather-App
+//
+//  Created by Paquin, Wyatt A on 2/27/18.
+//  Copyright © 2018 Paquin, Wyatt A. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS-Weather-App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS-Weather-App
+//
+//  Created by Paquin, Wyatt A on 2/27/18.
+//  Copyright © 2018 Paquin, Wyatt A. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
